40d5b9637f32432f402c4b3abcf0e90804e94b7f
Hiroyuki Tominaga <hiroyuki-t1161@berkeley.edu>
2019-10-26 11:47:37 -0700

    scraped images added

---
+++ data/style_images/naruto_style/scrape_from_video.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+def save_frame_range_sec(video_path, start_sec, stop_sec, step_sec,
+                         dir_path, basename, ext='jpg'):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fps_inv = 1 / fps
+
+    sec = start_sec
+    while sec < stop_sec:
+        n = round(fps * sec)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, n)
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite(
+                '{}_{}_{:.2f}.{}'.format(
+                    base_path, str(n).zfill(digit), n * fps_inv, ext
+                ),
+                frame
+            )
+        else:
+            return
+        sec += step_sec
+
+save_frame_range_sec('naruto.mp4', 17, 600, 1, 'scraped_image', 'naruto_img')
